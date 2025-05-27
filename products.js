794f12baf056da8a8fdf2b66b7acdf0976496509
shandor-torhac <shandorliccatorhac@gmail.com>
2025-05-27 19:33:20 -0300

    02 | html add class show-all | add scripts | css add justify-content center e align-items center na ul | add products.js e scripts.js

---
+++ products.js
@@ -0,0 +1,13 @@
+const menuOptions = [
+  { name: "X-Salada", price: 25, vegan: false, src: "./img/xsalada.jpeg" },
+  { name: "X-Bacon", price: 27, vegan: false, src: "./img/xbacon.png" },
+  { name: "X-Bacon Egg", price: 30, vegan: false, src: "./img/bacon-egg.png" },
+  { name: "Monstruoso", price: 32, vegan: false, src: "./img/monstruoso.png" },
+  { name: "Big Vegano", price: 30, vegan: false, src: "./img/xvegan.png" },
+  {
+    name: "X-Vegan",
+    price: 27,
+    vegan: false,
+    src: "./img/monstruoso-vegan.png",
+  },
+];
