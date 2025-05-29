5826a759550488da4a66a78489601a06de095500
shandor-torhac <shandorliccatorhac@gmail.com>
2025-05-29 14:13:58 -0300

    03 | html button add class | products.js mudança de false para true e duas linhas |  scripts.js alterações na estrutura.

--- products.js
+++ products.js
@@ -3,11 +3,11 @@ const menuOptions = [
   { name: "X-Bacon", price: 27, vegan: false, src: "./img/xbacon.png" },
   { name: "X-Bacon Egg", price: 30, vegan: false, src: "./img/bacon-egg.png" },
   { name: "Monstruoso", price: 32, vegan: false, src: "./img/monstruoso.png" },
-  { name: "Big Vegano", price: 30, vegan: false, src: "./img/xvegan.png" },
+  { name: "Big Vegano", price: 30, vegan: true, src: "./img/xvegan.png" },
   {
     name: "X-Vegan",
     price: 27,
-    vegan: false,
+    vegan: true,
     src: "./img/monstruoso-vegan.png",
   },
 ];
